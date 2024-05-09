c8a6ebabb6154809abb7f0f50d2c5c028134c704
Maaz_ahmed <maazwaqar076@gmail.com>
2024-05-09 18:53:35 +0500

    new work

--- index.ts
+++ index.ts
@@ -3,89 +3,47 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
   
-// welcome message with chalkANimation
-async function welcomeMessage() {
-    console.log(chalk.bold.hex('#DEADED').bold('Develop By Maaz Ahmed'));
-}
+const exchangeRate : any = {
+    USD: 1, 
+    GBP: 0.76, 
+    EURO: 0.90,
+    INR: 74, 
+    PKR: 279
+};
 
-await welcomeMessage()
 
-
-async function userAuth() {
-   
-    let randomId = Math.floor(Math.random() * 4135)
-    console.log(`Your Id is ${randomId}`)
-    let randomPin = Math.floor(Math.random() * 6028)
-    console.log(`Your pin is ${randomPin}`)
-    let isAuth = false;
-    while (!isAuth) {
-       const answers = await inquirer 
-    .prompt([
+let user_Choice = await inquirer .prompt(
+    [
         {
-            type: 'input',
-            name : 'userId',
-            message: 'please enter you id',
+            type: 'list',
+            name: 'formCurrency',
+            message: 'Select currency to convert',
+            choices:Object.keys(exchangeRate)
+
+        },
+        {
+            type: 'list',
+            name: 'toCurrency',
+            message: 'Select currency to covert in',
+            choices:Object.keys(exchangeRate)
+
         },
         {
             type: 'number',
-            name : 'userPin',
-            message: 'please enter you pin'
-        },
-    ]);
-    const parsedUserID = parseInt(answers.userId)
-    const parsedUserPin = parseInt(answers.userPin)
+            name: 'amount',
+            message: 'enter amount to convert',
+            validate: (input) => !isNaN(input) ? true : 'please enter a valid number'
+        }
+    ]
+)
 
-    if 
-    (parsedUserID === randomId  && parsedUserPin === randomPin)
-    {
-        console.log(chalk.green("Authentication successful! Welcome to the ATM."));
-        isAuth = true;
-    }
-    else{
-        console.log(chalk.red('Invalid user ID or PIN. Please try again.'))
-    }
-    }
-    
-    
-}
+let from = exchangeRate[user_Choice.formCurrency]
+let toAmount = exchangeRate[user_Choice.toCurrency]
+let amount = user_Choice.amount
+let baseAmount = amount / from 
+let convertedAmount = baseAmount * toAmount
+
+console.log(chalk.yellow(`the converstion rate of ${from} to ${toAmount} is`))
+console.log(chalk.green(convertedAmount))
 
 
-
-async function atmFun() {
-   await userAuth();
-    let replys = await inquirer .prompt([
-        {
-           type: 'list',
-            name : 'accType',
-            choices:["Saving","current"],
-           message: 'Select your account type'
-       },
-       {
-           type: 'list',
-           name : 'transactionType',
-           choices:["FastCash","withdrawal"],
-           message: 'Select your transaction type',
-           when: replys => replys.accType,
-       
-       },
-       {
-           type: 'list',
-           name : 'amount',
-           choices:["1000","5000", "10000"],
-           message: 'Select your amount',
-           when : replys => replys.transactionType === "FastCash",
-       },
-       {
-           type: 'number',
-           name : 'amount',
-           message: 'Select your amount',
-           when : replys => replys.transactionType === "withdrawal",
-       },
-   ]);
-   if (replys.transactionType === "withdrawal" || "FastCash") {
-       console.log(`You have withdrawn ${replys.amount} from your account`)
-   }
-
-}
-
-atmFun();
